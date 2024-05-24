5b415b06ceb95af827b66a085cd849891aa01f5a
sansan88 <sandro.scalco@liitu.ch>
2024-05-24 12:06:38 +0200

    Comments

--- src/importer.ts
+++ src/importer.ts
@@ -1,14 +1,17 @@
 // Import mastodon
 import Mastodon from 'mastodon-api'
 
-// FIle and other Nodejs stuff
+// File and other Nodejs stuff
 const fs = require('node:fs');
 
 // Env Variables
 require('dotenv').config();
 
-// TWitter Profile
+// Read Twitter/X Profile
 
+
+
+// INIT Mastodon
 const M = new Mastodon({
   access_token: process.env.Token,
   timeout_ms: 60*1000,  // optional HTTP request timeout to apply to all requests.
